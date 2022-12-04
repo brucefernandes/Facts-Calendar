cf7a7ae2f88e0942a4a3c862e9b40e0d7265045c
Bruce Brad Fernandes <brucef300@gmail.com>
2022-12-04 17:03:49 -0500

    Updates

---
+++ precache-manifest.cc12c9f2aa56b7c579c1961029abfe69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8b0148f9d56cce492e8",
+    "url": "/Facts-Calendar/static/css/main.2077399e.chunk.css"
+  },
+  {
+    "revision": "b8b0148f9d56cce492e8",
+    "url": "/Facts-Calendar/static/js/main.b8b0148f.chunk.js"
+  },
+  {
+    "revision": "86bacfac8abc43da1d5f",
+    "url": "/Facts-Calendar/static/js/1.86bacfac.chunk.js"
+  },
+  {
+    "revision": "e1b10ebaa4b831e5e7a0",
+    "url": "/Facts-Calendar/static/js/runtime~main.e1b10eba.js"
+  },
+  {
+    "revision": "7824cf5f0507d0c1b4e3d5b3e22e93a8",
+    "url": "/Facts-Calendar/index.html"
+  }
+];
